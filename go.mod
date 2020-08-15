35c2dc2e6dc90aee49fc0a9f20f169d6c349d791
Horace Lee <horacehylee2@gmail.com>
2020-08-15 21:59:42 +0800

    add chunk logic for streaming

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-playground/validator/v10 v10.3.0
 	github.com/google/go-cmp v0.5.1
 	github.com/matryer/moq v0.0.0-20200607124540-4638a53893e6
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 )
--- pkg/youtube/downloader.go
+++ pkg/youtube/downloader.go
@@ -1,12 +1,20 @@
 package youtube
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"math"
 	"strings"
 
 	"github.com/horacehylee/go-youtube-dl/pkg/youtube/client"
 	"github.com/horacehylee/go-youtube-dl/pkg/youtube/decipher"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	streamChunkSize = int64(10 * 1024 * 1024) // 10MB for stream URL chuck
 )
 
 // Downloader for downloading Youtube videos
@@ -45,21 +53,22 @@ func (d *Downloader) DownloadAudio(videoID string, w io.Writer) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("url: %v\n", url)
 
 	length, err := d.client.StreamLength(url)
 	if err != nil {
 		return err
 	}
 
-	r, err := d.client.Stream(url, 0, length)
+	bytes, err := d.readStreamChunks(url, length)
 	if err != nil {
 		return err
 	}
-	defer r.Close()
 
-	_, err = io.Copy(w, r)
-	return err
+	bufWriter := bufio.NewWriterSize(w, int(streamChunkSize))
+	for _, b := range bytes {
+		bufWriter.Write(b)
+	}
+	return bufWriter.Flush()
 }
 
 func (d *Downloader) getURL(videoID string, s client.StreamFormat) (string, error) {
@@ -70,14 +79,43 @@ func (d *Downloader) getURL(videoID string, s client.StreamFormat) (string, erro
 		return s.URL, nil
 	}
 	cipher := decipher.NewDecipher(d.client)
-	url, err := cipher.StreamURL(videoID, s.SignatureCipher)
-	if err != nil {
-		return "", err
+	return cipher.StreamURL(videoID, s.SignatureCipher)
+}
+
+func (d *Downloader) readStreamChunks(url string, length int64) ([][]byte, error) {
+	chunks := int(math.Ceil(float64(length) / float64(streamChunkSize)))
+	bytes := make([][]byte, chunks)
+	var g errgroup.Group
+
+	from := int64(0)
+	for i := 0; i < chunks; i++ {
+		to := from + streamChunkSize
+		if to > length {
+			to = length
+		}
+
+		g.Go(func(i int, from int64, to int64) func() error {
+			return func() error {
+				r, err := d.client.Stream(url, from, to)
+				if err != nil {
+					return err
+				}
+				defer r.Close()
+
+				b, err := ioutil.ReadAll(r)
+				if err != nil {
+					return err
+				}
+				bytes[i] = b
+				return nil
+			}
+		}(i, from, to))
+
+		from = to + 1
 	}
-	if url == "" {
-		return "", fmt.Errorf("url cannot be empty")
-	}
-	return url, nil
+
+	err := g.Wait()
+	return bytes, err
 }
 
 type streamPredicate = func(s client.StreamFormat) bool
